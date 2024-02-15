976e5fdd97d87c1d578b75248b42772b96d3813e
Tyson Pham <tyson@tysons-air.home>
2024-02-15 18:26:34 -0400

    fixing useHistory in single product card component, then fix bugs in product page, then refactor to remove repeated code

--- client/src/components/card/single-product-card.component.jsx
+++ client/src/components/card/single-product-card.component.jsx
@@ -1,202 +1,202 @@
-// import React from "react";
-// import { useSelector, useDispatch } from "react-redux";
-// import { Card, Tabs } from "antd";
-// import { Link, useHistory, useParams } from "react-router-dom";
-// import {
-// 	HeartOutlined,
-// 	ShoppingCartOutlined,
-// 	StarOutlined,
-// 	CheckOutlined,
-// 	HeartFilled,
-// } from "@ant-design/icons";
-// import { CartActionTypes } from "./../../redux/reducers/cart/cart.types";
-// import techdevices from "./../images/techdevices.jpeg";
-// import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-// import { Carousel } from "react-responsive-carousel";
-// import { ModalComponent } from "./../../components/modal/modal.component";
-// import { ProductInfoCard } from "./../card/product-info-card.component";
-// import { ShowAverage } from "./../rating/average-rating.component";
-// import StarRatings from "react-star-ratings";
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Card, Tabs } from "antd";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import {
+	HeartOutlined,
+	ShoppingCartOutlined,
+	StarOutlined,
+	CheckOutlined,
+	HeartFilled,
+} from "@ant-design/icons";
+import { CartActionTypes } from "./../../redux/reducers/cart/cart.types";
+import techdevices from "./../images/techdevices.jpeg";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import { ModalComponent } from "./../../components/modal/modal.component";
+import { ProductInfoCard } from "./../card/product-info-card.component";
+import { ShowAverage } from "./../rating/average-rating.component";
+import StarRatings from "react-star-ratings";
 
-// const { TabPane } = Tabs;
+const { TabPane } = Tabs;
 
-// export const SingleProduct = ({
-// 	wishList,
-// 	user,
-// 	star,
-// 	handleStarRating,
-// 	product,
-// 	handleAddToWishList,
-// }) => {
-// 	const { images, title, description } = product;
-// 	const dispatch = useDispatch();
+export const SingleProduct = ({
+	wishList,
+	user,
+	star,
+	handleStarRating,
+	product,
+	handleAddToWishList,
+}) => {
+	const { images, title, description } = product;
+	const dispatch = useDispatch();
 
-// 	const cartItems = useSelector((state) => state.cart.cartItems);
-// 	const history = useHistory();
-// 	const param = useParams();
+	const cartItems = useSelector((state) => state.cart.cartItems);
+	const navigate = useNavigate();
+	const param = useParams();
 
-// 	const showUserRatingModal = () => {
-// 		if (user) {
-// 			return (
-// 				<>
-// 					<ModalComponent
-// 						button={<StarOutlined className="text-danger" />}
-// 						title="Leave a rating"
-// 						user={user}
-// 						rating={
-// 							<StarRatings
-// 								rating={star}
-// 								starRatedColor="blue"
-// 								changeRating={(e) => handleStarRating(e)}
-// 								numberOfStars={5}
-// 								starHoverColor="blue"
-// 								name="rating"
-// 							/>
-// 						}
-// 					/>
-// 				</>
-// 			);
-// 		} else {
-// 			return (
-// 				<div onClick={(e) => handleHistory(e)}>
-// 					<StarOutlined className="text-danger" />
-// 					<br />
-// 					Log in to leave a rating
-// 				</div>
-// 			);
-// 		}
-// 	};
-// 	const checkItemInCart = () => {
-// 		const itemInCart = cartItems.find(
-// 			(cartItem) => cartItem._id === product._id
-// 		);
-// 		if (itemInCart) {
-// 			return (
-// 				<Link to={`/cart`}>
-// 					<CheckOutlined disabled={true} />
-// 					<br />
-// 					Item added to cart
-// 				</Link>
-// 			);
-// 		} else if (product.quantity === 0) {
-// 			return (
-// 				<>
-// 					<ShoppingCartOutlined disabled={true} className="text-warning" />
-// 					<br />
-// 					<p className="text-danger">Out of Stock</p>
-// 				</>
-// 			);
-// 		} else {
-// 			return (
-// 				<>
-// 					<ShoppingCartOutlined
-// 						onClick={handleAddToCart}
-// 						className="text-warning"
-// 					/>
-// 					<br />
-// 					Add to cart
-// 				</>
-// 			);
-// 		}
-// 	};
-// 	const checkItemInWishlist = () => {
-// 		if (!user) {
-// 			return (
-// 				<Link to={`/login`}>
-// 					<HeartOutlined
-// 						className="text-danger"
-// 						onClick={() => handleAddToWishList()}
-// 					/>
-// 					<br />
-// 					Login to add product to Wishlist
-// 				</Link>
-// 			);
-// 		} else {
-// 			const itemInWishList = wishList.find((item) => item._id === product._id);
-// 			if (itemInWishList) {
-// 				return (
-// 					<Link to={`/user/wishlist`}>
-// 						<HeartFilled className="text-danger" />
-// 						<br />
-// 						Go to Wishlist
-// 					</Link>
-// 				);
-// 			} else {
-// 				return (
-// 					<>
-// 						<HeartOutlined
-// 							className="text-danger"
-// 							onClick={() => handleAddToWishList()}
-// 						/>
-// 						<br />
-// 						Add to Wishlist
-// 					</>
-// 				);
-// 			}
-// 		}
-// 	};
+	const showUserRatingModal = () => {
+		if (user) {
+			return (
+				<>
+					<ModalComponent
+						button={<StarOutlined className="text-danger" />}
+						title="Leave a rating"
+						user={user}
+						rating={
+							<StarRatings
+								rating={star}
+								starRatedColor="blue"
+								changeRating={(e) => handleStarRating(e)}
+								numberOfStars={5}
+								starHoverColor="blue"
+								name="rating"
+							/>
+						}
+					/>
+				</>
+			);
+		} else {
+			return (
+				<div onClick={(e) => handleHistory(e)}>
+					<StarOutlined className="text-danger" />
+					<br />
+					Log in to leave a rating
+				</div>
+			);
+		}
+	};
+	const checkItemInCart = () => {
+		const itemInCart = cartItems.find(
+			(cartItem) => cartItem._id === product._id
+		);
+		if (itemInCart) {
+			return (
+				<Link to={`/cart`}>
+					<CheckOutlined disabled={true} />
+					<br />
+					Item added to cart
+				</Link>
+			);
+		} else if (product.quantity === 0) {
+			return (
+				<>
+					<ShoppingCartOutlined disabled={true} className="text-warning" />
+					<br />
+					<p className="text-danger">Out of Stock</p>
+				</>
+			);
+		} else {
+			return (
+				<>
+					<ShoppingCartOutlined
+						onClick={handleAddToCart}
+						className="text-warning"
+					/>
+					<br />
+					Add to cart
+				</>
+			);
+		}
+	};
+	const checkItemInWishlist = () => {
+		if (!user) {
+			return (
+				<Link to={`/login`}>
+					<HeartOutlined
+						className="text-danger"
+						onClick={() => handleAddToWishList()}
+					/>
+					<br />
+					Login to add product to Wishlist
+				</Link>
+			);
+		} else {
+			const itemInWishList = wishList.find((item) => item._id === product._id);
+			if (itemInWishList) {
+				return (
+					<Link to={`/user/wishlist`}>
+						<HeartFilled className="text-danger" />
+						<br />
+						Go to Wishlist
+					</Link>
+				);
+			} else {
+				return (
+					<>
+						<HeartOutlined
+							className="text-danger"
+							onClick={() => handleAddToWishList()}
+						/>
+						<br />
+						Add to Wishlist
+					</>
+				);
+			}
+		}
+	};
 
-// 	const handleAddToCart = () => {
-// 		dispatch({
-// 			type: CartActionTypes.ADD_TO_CART,
-// 			payload: product,
-// 		});
-// 		dispatch({
-// 			type: CartActionTypes.TOGGLE_DRAWER,
-// 		});
-// 	};
-// 	const handleHistory = (e) => {
-// 		e.preventDefault();
-// 		history.push({
-// 			pathname: "/login",
-// 			state: { from: `/product/${param.slug}` },
-// 		});
-// 	};
-// 	return (
-// 		<>
-// 			<div className="col-md-7">
-// 				{images && images.length ? (
-// 					<Carousel showArrows={true} infiniteLoop>
-// 						{images.map((images) => {
-// 							return (
-// 								<img src={images.url} key={images.public_id} alt="Product" />
-// 							);
-// 						})}
-// 					</Carousel>
-// 				) : (
-// 					<img
-// 						src={techdevices}
-// 						alt="defalut product"
-// 						style={{ height: "600px", width: "100%", objectFit: "contain" }}
-// 					/>
-// 				)}
-// 				<Tabs type="card">
-// 					<TabPane tab="Description" key="1">
-// 						{description ? description : ""}
-// 					</TabPane>
-// 					<TabPane tab="More" key="2">
-// 						Call use on xxx xxx xxxx to learn more about this product
-// 					</TabPane>
-// 				</Tabs>
-// 			</div>
-// 			<div className="col-md-5">
-// 				<h2 className="text-center">{title}</h2>
-// 				{product && product.ratings && product.ratings.length ? (
-// 					<ShowAverage className="pb-4 " product={product} />
-// 				) : (
-// 					<div className="text-center pb-3">No ratings yet</div>
-// 				)}
+	const handleAddToCart = () => {
+		dispatch({
+			type: CartActionTypes.ADD_TO_CART,
+			payload: product,
+		});
+		dispatch({
+			type: CartActionTypes.TOGGLE_DRAWER,
+		});
+	};
+	const handleHistory = (e) => {
+		e.preventDefault();
+		navigate({
+			pathname: "/login",
+			state: { from: `/product/${param.slug}` },
+		});
+	};
+	return (
+		<>
+			<div className="col-md-7">
+				{images && images.length ? (
+					<Carousel showArrows={true} infiniteLoop>
+						{images.map((images) => {
+							return (
+								<img src={images.url} key={images.public_id} alt="Product" />
+							);
+						})}
+					</Carousel>
+				) : (
+					<img
+						src={techdevices}
+						alt="defalut product"
+						style={{ height: "600px", width: "100%", objectFit: "contain" }}
+					/>
+				)}
+				<Tabs type="card">
+					<TabPane tab="Description" key="1">
+						{description ? description : ""}
+					</TabPane>
+					<TabPane tab="More" key="2">
+						Call use on xxx xxx xxxx to learn more about this product
+					</TabPane>
+				</Tabs>
+			</div>
+			<div className="col-md-5">
+				<h2 className="text-center">{title}</h2>
+				{product && product.ratings && product.ratings.length ? (
+					<ShowAverage className="pb-4 " product={product} />
+				) : (
+					<div className="text-center pb-3">No ratings yet</div>
+				)}
 
-// 				<Card
-// 					actions={[
-// 						<>{checkItemInCart()}</>,
-// 						checkItemInWishlist(),
-// 						showUserRatingModal(),
-// 					]}
-// 				>
-// 					<ProductInfoCard product={product} />
-// 				</Card>
-// 			</div>
-// 		</>
-// 	);
-// };
+				<Card
+					actions={[
+						<>{checkItemInCart()}</>,
+						checkItemInWishlist(),
+						showUserRatingModal(),
+					]}
+				>
+					<ProductInfoCard product={product} />
+				</Card>
+			</div>
+		</>
+	);
+};
--- client/src/pages/product/product.component.jsx
+++ client/src/pages/product/product.component.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import {
-  getOneProduct,
-  getRelatedProducts,
-  relatedProductsCount,
+	getOneProduct,
+	getRelatedProducts,
+	relatedProductsCount,
 } from "./../../utils/products/products.utils";
 import { addToWishList, getAllWishList } from "./../../utils/user/user.utils";
 import { SingleProduct } from "./../../components/card/single-product-card.component";
@@ -11,104 +12,117 @@ import { ProductCard } from "./../../components/card/regular.product-card.compon
 import { updateStarRating } from "./../../utils/products/products.utils";
 import { Pagination } from "antd";
 import { toast } from "react-toastify";
-export const ProductPage = ({ match }) => {
-  const [product, setProduct] = useState({});
-  const [page, setPage] = useState(1);
-  const [wishList, setWishList] = useState([]);
-  const [relatedProducts, setRelatedProducts] = useState([]);
-  const [productsQuantity, setProductsQuantity] = useState(0);
-  const [star, setStar] = useState();
-  const { slug } = match.params;
-  const user = useSelector((state) => state.user.currentUser);
-  useEffect(() => {
-    loadProduct();
-  }, [slug, page]);
-  //Note this
-  useEffect(() => {
-    loadRelatedProduct();
-    loadRelatedPorductCount();
-    loadWishList();
-  }, [product]);
-  useEffect(() => {
-    loadWishList();
-  }, [user]);
-  const loadWishList = async () => {
-    await getAllWishList(user.token).then((res) => {
-      console.log(res.data.wishList);
-      setWishList(res.data.wishList);
-    });
-  };
-  const handleAddToWishList = () => {
-    addToWishList(user.token, product._id)
-      .then((res) => {
-        toast.success("Product added to wish list");
-        loadWishList();
-      })
-      .catch((err) => console.log(err));
-  };
-  const loadProduct = async () => {
-    await getOneProduct(slug)
-      .then((res) => {
-        setProduct(res.data);
-      })
-      .catch((err) => console.log(err));
-  };
-  const loadRelatedPorductCount = async () => {
-    if (product && product.category) {
-      await relatedProductsCount(product.category._id)
-        .then((res) => setProductsQuantity(res.data))
-        .catch((err) => console.log(err));
-    }
-  };
-  const loadRelatedProduct = async () => {
-    if (product && product.category) {
-      await getRelatedProducts(slug, product.category._id, page)
-        .then((res) => {
-          setRelatedProducts(res.data);
-        })
-        .catch((err) => console.log(err));
-    }
-  };
-  const handleStarRating = async (e) => {
-    setStar(e);
-    await updateStarRating(product._id, user.token, e)
-      .then()
-      .catch((err) => console.log(err));
-  };
 
-  return (
-    <div className="container-fluid">
-      <div className="row pt-3">
-        <SingleProduct
-          loadWishList={loadWishList}
-          wishList={wishList}
-          star={star}
-          handleAddToWishList={handleAddToWishList}
-          handleStarRating={handleStarRating}
-          product={product}
-          user={user}
-        />
-      </div>
-      <div className="row mt-5 text-center">
-        <h4 className="text-center pt-3 pb-5 display-7 jumbotron">
-          Related Product
-        </h4>
-        {relatedProducts.map((relatedProduct) => {
-          return (
-            <div className="col-md-4" key={relatedProduct._id}>
-              <ProductCard product={relatedProduct} />
-            </div>
-          );
-        })}
-        <Pagination
-          className="text-center pt-3"
-          current={page}
-          total={Math.round((productsQuantity / 3) * 10)}
-          onChange={(value) => {
-            setPage(value);
-          }}
-        />
-      </div>
-    </div>
-  );
+export const ProductPage = () => {
+	const [product, setProduct] = useState({});
+	const [page, setPage] = useState(1);
+	const [wishList, setWishList] = useState([]);
+	const [relatedProducts, setRelatedProducts] = useState([]);
+	const [productsQuantity, setProductsQuantity] = useState(0);
+	const [star, setStar] = useState();
+	const { slug } = useParams();
+	const user = useSelector((state) => state.user.currentUser);
+
+	useEffect(() => {
+		loadProduct();
+		loadWishList();
+	}, [slug, page, user]);
+
+	useEffect(() => {
+		loadRelatedProduct();
+		loadRelatedProductCount();
+	}, [product]);
+
+	const loadWishList = async () => {
+		try {
+			const res = await getAllWishList(user.token);
+			setWishList(res.data.wishList);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const handleAddToWishList = async () => {
+		try {
+			await addToWishList(user.token, product._id);
+			toast.success("Product added to wish list");
+			loadWishList();
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const loadProduct = async () => {
+		try {
+			const res = await getOneProduct(slug);
+			setProduct(res.data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const loadRelatedProductCount = async () => {
+		try {
+			if (product && product.category) {
+				const res = await relatedProductsCount(product.category._id);
+				setProductsQuantity(res.data);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const loadRelatedProduct = async () => {
+		try {
+			if (product && product.category) {
+				const res = await getRelatedProducts(slug, product.category._id, page);
+				setRelatedProducts(res.data);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const handleStarRating = async (e) => {
+		try {
+			setStar(e);
+			await updateStarRating(product._id, user.token, e);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	return (
+		<div className="container-fluid">
+			<div className="row pt-3">
+				<SingleProduct
+					loadWishList={loadWishList}
+					wishList={wishList}
+					star={star}
+					handleAddToWishList={handleAddToWishList}
+					handleStarRating={handleStarRating}
+					product={product}
+					user={user}
+				/>
+			</div>
+			<div className="row mt-5 text-center">
+				<h4 className="text-center pt-3 pb-5 display-7 jumbotron">
+					Related Product
+				</h4>
+				{relatedProducts.map((relatedProduct) => (
+					<div className="col-md-4" key={relatedProduct._id}>
+						<ProductCard product={relatedProduct} />
+					</div>
+				))}
+				<Pagination
+					className="text-center pt-3"
+					current={page}
+					total={Math.ceil(productsQuantity / 3) * 10}
+					onChange={(value) => {
+						setPage(value);
+					}}
+				/>
+			</div>
+		</div>
+	);
 };
