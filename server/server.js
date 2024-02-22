a86177ac172692acf11bb124520e1f3780ed78ac
Tyson Pham <tyson@tysons-air.home>
2024-02-22 19:59:41 -0400

    building front end and add to backend

--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ app.get("/*", (req, res) => {
 
 //3 APP LISTENS TO PORT
 //port
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`listen to server running on port ${port}`));
 
 // const proxy = require("http-proxy-middleware");
